20cc889f6cb237d5cbb533fdd63f2cf20fea5dba
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-10-18 18:17:48 +0400

    Started realize CRUD operations - added GET

---
+++ src/main/java/ru/tutorial/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.tutorial.dao;
+
+import org.springframework.stereotype.Component;
+import ru.tutorial.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<>();
+        people.add(new Person(++PEOPLE_COUNT, "Katty"));
+        people.add(new Person(++PEOPLE_COUNT, "Sally"));
+        people.add(new Person(++PEOPLE_COUNT, "Frank"));
+        people.add(new Person(++PEOPLE_COUNT, "Colin"));
+        people.add(new Person(++PEOPLE_COUNT, "Stan"));
+    }
+
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+       return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
+    }
+}
