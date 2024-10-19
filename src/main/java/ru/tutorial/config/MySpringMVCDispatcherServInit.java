aea084d9884a858242409001e8ec45e1e025a83d
Alex Nikonov <60483531+lexxA87@users.noreply.github.com>
2024-10-19 17:10:06 +0400

    Added Patch and Delete methods

--- src/main/java/ru/tutorial/config/MySpringMVCDispatcherServInit.java
+++ src/main/java/ru/tutorial/config/MySpringMVCDispatcherServInit.java
@@ -1,6 +1,10 @@
 package ru.tutorial.config;
 
+import jakarta.servlet.ServletException;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import jakarta.servlet.ServletContext;
 
 public class MySpringMVCDispatcherServInit
         extends AbstractAnnotationConfigDispatcherServletInitializer {
@@ -17,5 +21,16 @@ public class MySpringMVCDispatcherServInit
     @Override
     protected String[] getServletMappings() {
         return new String[] {"/"};
+    }
+
+    @Override
+    public void onStartup(ServletContext aServletContext) throws ServletException {
+        super.onStartup(aServletContext);
+        registerHiddenFieldFilter(aServletContext);
+    }
+
+    private void registerHiddenFieldFilter(ServletContext aContext) {
+        aContext.addFilter("hiddenHttpMethodFilter",
+                new HiddenHttpMethodFilter()).addMappingForUrlPatterns(null ,true, "/*");
     }
 }
